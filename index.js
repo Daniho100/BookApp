44e80e3648412e357e3eff9e3ca4823ceba2366f
Ojiaku Daniel <daniandre.dah@gmail.com>
2024-09-03 20:53:09 +0100

    project deployment

--- index.js
+++ index.js
@@ -15,15 +15,31 @@ const __dirname = path.dirname(__filename)
 
 
 app.use(express.json());
+
+const allowedOrigins = ['http://localhost:3000', 'https://bookapp-32k1.onrender.com'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+}));
+
+
+
 // app.use(cors());
 // Option 2: Allow Custom Origins
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-  })
-);
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
 
 // app.get('/', (request, response) => {
 //   console.log(request);
