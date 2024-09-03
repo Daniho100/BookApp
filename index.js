b454290d598422768d3aa47aad3574973d3eb64e
Ojiaku Daniel <daniandre.dah@gmail.com>
2024-09-03 20:40:21 +0100

    project deployment

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ const __dirname = path.dirname(__filename)
 
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 // Option 2: Allow Custom Origins
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type'],
-//   })
-// );
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
 
 // app.get('/', (request, response) => {
 //   console.log(request);
