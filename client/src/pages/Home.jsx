ec30404ca17335b372c0660f69ffbd53885b07b0
Ojiaku Daniel <daniandre.dah@gmail.com>
2024-09-04 11:23:25 +0100

    done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get('https://localhost:5555/books')
+      .get('http://localhost:5555/books')
       .then((response) => {
         setBooks(response.data.datas);
         setLoading(false);
